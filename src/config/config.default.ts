f8c1ad9037cfb00dbdcff02b51a3cd8e7317a7a9
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-08-27 15:58:55 +0800

    ci: add env

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -5,7 +5,7 @@ import JSON5 from 'json5';
 export default (): MidwayConfig => {
   function getSignConfig(): SignConfig {
     try {
-      return JSON5.parse(process.env.signConfig);
+      return JSON5.parse(process.env.SIGN_CONFIG);
     } catch (e) {
       throw new Error('JSON5解析错误: ' + e.toString());
     }
